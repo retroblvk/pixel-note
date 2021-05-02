12ccab71009598daffbe40932f93ff42d805d36a
retro <retroblvk@gmail.com>
2021-05-02 15:01:51 -0600

    setup note functions, added in content and added in styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./css/main.css";
+import { useState } from "react";
 function App() {
+  const [notes, setNotes] = useState([]);
+  let [note, setNote] = useState("");
+  const handleChange = (e) => {
+    setNote(e.target.value);
+  };
+  const handleClick = (e) => {
+    setNotes((notes) => [...notes, note]);
+    setNote("");
+  };
+  const handleDelete = (e) => {
+    const filteredNotes = notes.filter(
+      (note, i) => i != e.target.parentNode.value
+    );
+    setNotes(filteredNotes);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Pixel Note</h1>
+      <input
+        type="text"
+        onChange={handleChange}
+        placeholder="Create Note"
+        value={note}
+      />
+      <button onClick={handleClick} className="submit">
+        Submit
+      </button>
+      <ul className="notes">
+        {notes.map((note, i) => {
+          return (
+            <li key={i} value={i}>
+              <span>{note}</span>{" "}
+              <button className="delete" onClick={handleDelete}>
+                X
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
